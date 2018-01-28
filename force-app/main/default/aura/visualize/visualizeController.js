24ebca305c1e79a4c74fc631b63abaf3a479daea
Dan Shahin <dan@codescience.com>
2018-01-28 11:46:48 -0800

    add lightning component tab

--- force-app/main/default/aura/visualize/visualizeController.js
+++ force-app/main/default/aura/visualize/visualizeController.js
@@ -1,5 +1,10 @@
 ({
+    doInit : function(component, event, helper) {
+        console.log('initialized');
+        alert('init');
+    },
     jsLoaded : function(component, event, helper) {
+        console.log('loaded js');
         alert('loaded js');
     }
 })
