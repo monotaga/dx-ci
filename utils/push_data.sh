57c47008b4e4ce798d30d712b6c5586038cc1a97
Dan Shahin <dan@codescience.com>
2018-02-01 16:41:18 -0800

    nuke and restore data

--- utils/push_data.sh
+++ utils/push_data.sh
@@ -8,9 +8,14 @@ then
     exit
 fi
 
+# create anonymous apex file to execute
 echo "delete [select id from Account];" > anon.apex
 
 # delete existing data
-sfdx force:apex:execute -f anon.apex
-#push data 
+sfdx force:apex:execute -f anon.apex -u $1
+
+# delete the anonymous apex
+rm anon.apex
+
+#push fresh dataset data 
 sfdx force:data:tree:import --sobjecttreefiles data/Account-Contact-Opportunity.json -u $1
